ee067f485e9c0226fd45e406a9ed7a96c72ac9fa
huggla <henrik.uggla@kristianstad.se>
2017-05-19 11:01:29 +0200

    Update entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,4 +5,18 @@ groupmod --new-name ${USER} user
 echo -n ${USER}':' >> /etc/shadow.org
 cat /etc/shadow.org /run/secrets/user-pw > /etc/shadow
 echo ':17304:0:99999:7:::' >> /etc/shadow
-dropbear -FREwp 2222
+rm -f /home/user/.ssh/authorized_keys
+if [ -e /run/secrets/ssh-authorized_keys ]
+then
+    cp /run/secrets/ssh-authorized_keys /home/user/.ssh/authorized_keys
+else
+    touch /run/secrets/ssh-authorized_keys
+fi
+if [ -e /run/secrets/ssh-themeupdate_key ]
+then
+    echo >> /run/secrets/ssh-authorized_keys
+    echo -n 'no-port-forwarding,no-agent-forwarding,no-X11-forwarding,no-pty,command="/usr/local/bin/upd-qwc2-themes" >> /run/secrets/ssh-authorized_keys
+    cat /run/secrets/ssh-themeupdate_key >> /run/secrets/ssh-authorized_keys
+fi
+chmod u=rw,go= /run/secrets/ssh-authorized_keys
+dropbear -FREsjkmwp 2222
