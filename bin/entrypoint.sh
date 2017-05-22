7245b3f4e376aaf757a268ea890189eddbee0dae
huggla <henrik.uggla@kristianstad.se>
2017-05-22 09:40:39 +0200

    Update entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,23 +2,28 @@
 
 usermod --login ${USER} user
 groupmod --new-name ${USER} user
-echo -n ${USER}':' >> /etc/shadow.org
-cat /etc/shadow.org /run/secrets/user-pw > /etc/shadow
-echo ':17304:0:99999:7:::' >> /etc/shadow
-rm -f /home/user/.ssh/authorized_keys
-if [ -e /run/secrets/ssh-authorized_keys ]
+if [ -s /run/secrets/user-pw ]
 then
-    cp /run/secrets/ssh-authorized_keys /home/user/.ssh/authorized_keys
-    echo >> /home/user/.ssh/authorized_keys
-else
-    touch /home/user/.ssh/authorized_keys
-fi
-if [ -e /run/secrets/ssh-themeupdate-key ]
+    echo -n ${USER}':' >> /etc/shadow.org
+    cat /etc/shadow.org /run/secrets/user-pw > /etc/shadow
+    echo ':17304:0:99999:7:::' >> /etc/shadow
+    dropbear -FREjkmwp ${SSH-PORT}
+elif [ -s /run/secrets/ssh-authorized_keys ] || [ -s /run/secrets/ssh-themeupdate-key ]
 then
-    echo -n 'no-port-forwarding,no-agent-forwarding,no-X11-forwarding,no-pty,command="/usr/local/bin/upd-qwc2-themes" ' >> /home/user/.ssh/authorized_keys
-    cat /run/secrets/ssh-themeupdate-key >> /home/user/.ssh/authorized_keys
-    echo >> /home/user/.ssh/authorized_keys
+    if [ -s /run/secrets/ssh-authorized_keys ]
+    then
+        cp /run/secrets/ssh-authorized_keys /home/user/.ssh/authorized_keys
+        echo >> /home/user/.ssh/authorized_keys
+    else
+        > /home/user/.ssh/authorized_keys
+    fi
+    if [ -s /run/secrets/ssh-themeupdate-key ]
+    then
+        echo -n 'no-port-forwarding,no-agent-forwarding,no-X11-forwarding,no-pty,command="/usr/local/bin/upd-qwc2-themes" ' >> /home/user/.ssh/authorized_keys
+        cat /run/secrets/ssh-themeupdate-key >> /home/user/.ssh/authorized_keys
+        echo >> /home/user/.ssh/authorized_keys
+    fi
+    chown ${USER}:${USER} /home/user/.ssh/authorized_keys
+    chmod u=rw,go= /home/user/.ssh/authorized_keys
+    dropbear -FREsjkmwp ${SSH-PORT}
 fi
-chown ${USER}:${USER} /home/user/.ssh/authorized_keys
-chmod u=rw,go= /home/user/.ssh/authorized_keys
-dropbear -FREsjkmwp 2222
